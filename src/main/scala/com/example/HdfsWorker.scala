24538eb20aa70763c752aac60bb7a3813d95c768
Ilia Onishchenko <ilia_onishchenko@epam.com>
2017-08-16 20:26:46 +0300

    Refactor code

--- src/main/scala/com/example/HdfsWorker.scala
+++ src/main/scala/com/example/HdfsWorker.scala
@@ -5,16 +5,12 @@ import java.io._
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 
-import scala.collection.mutable
-import scala.concurrent.ExecutionContext
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
 
 object HdfsWorker extends App {
 
-
   var path = new Path("/user/ilia/firstTask")
-  println("Start working, path is: "+path.toString)
+  println("Start working, path is: " + path.toString)
 
   if (args.length == 1)
     path = new Path(args(0))
@@ -25,55 +21,20 @@ object HdfsWorker extends App {
 
   val files = fs.listStatus(path)
 
-  var map = mutable.HashMap[String, Int]().withDefaultValue(0)
-  val p = "^.*?(?=\\t)".r
-  for {
-    file <- files
-    lines = Source
+  private val map: Map[String, Int] = (for {file <- files} yield {
+    Source
       .fromInputStream(fs.open(file.getPath))
       .getLines()
       .map(line => line.split("\t")(2))
-      .foldLeft(mutable.HashMap.empty[String, Int].withDefaultValue(0))((acc, x) => {acc(x) += 1; acc})
-  } yield {
-    println("looking file: " + file.getPath.toString)
-    val newPath = file.getPath.toString.split(".bid.")(0) + "/result/"
-    writeToFile(lines, new Path(newPath + file.getPath.toString.split(".bid.")(1)))
-    lines.clear()
-    println(file.getPath.toString)
-  }
+      .toList
+      .groupBy(x => x)
+      .mapValues(_.size)
+      .toList
+  }).flatten.groupBy(t => t._1).mapValues(list => list.map(t => t._2).sum)
 
-  path = new Path(path.toString + "/result/")
-  val results = fs.listStatus(path)
-  val pattern = "\\((.*),([^,]+)\\)$".r
+  writeSeqToFile(map.toSeq.sortWith(_._2 > _._2), new Path(path + "/result.txt"))
 
-  for {
-    file <- results
-    lines = Source
-      .fromInputStream(fs.open(file.getPath))
-      .getLines()
-      .map(line => { val pattern(key, value) = line; (key, value) })
-  } yield {
-    println("looking file2: " + file.getPath.toString)
-    lines.foreach(line => map(line._1) += line._2.toInt)
-  }
-
-  writeToFile(map.toSeq.sortWith(_._2 > _._2), new Path(path + "/result.txt"))
-
-  def counts[String](xs: Iterator[String]): mutable.Map[String, Int] = {
-    xs.foldLeft(mutable.HashMap.empty[String, Int].withDefaultValue(0))(
-      (acc, x) => {
-        acc(x) += 1; acc
-      })
-  }
-
-  def writeToFile(seq: Seq[(String, Int)], fileName: Path)(implicit ec: ExecutionContext) = {
-    val writer = new PrintWriter(fs.create(fileName))
-    for (line <- seq) {
-      writer.write(line + "\n")
-    }
-    writer.close()
-  }
-  def writeToFile(map: mutable.Map[String, Int], fileName: Path)(implicit ec: ExecutionContext) = {
+  def writeSeqToFile(map: Seq[(String, Int)], fileName: Path) = {
     val writer = new PrintWriter(fs.create(fileName))
     for (line <- map) {
       writer.write(line + "\n")
